70f1a658546adc51759b661ba2c91ca72ea0cd74
Alla2804 <al.la.nov.28@gmail.com>
2021-04-18 17:42:02 +0300

    File Cats.jsx updated. File OneCats.jsx is created and showed the first post from the base (nickname, text, 5 photo). one article and list output

--- src/components/Cats.jsx
+++ src/components/Cats.jsx
@@ -5,9 +5,9 @@ import {NavLink} from "react-router-dom";
 function Div(props){
     return <div className="col-lg-3 col-md-6">
         <div className="single-cat-list" >
-            <NavLink to="/OneCat">
-                <h3><p style={{fontSize:18,color:"#8c0494",fontFamily:"Georgia" }}>{props.nickname}</p></h3>
-                <img className="img-fluid" src={props.photo_1} alt="cat" style={{width:262,height:330}}/>
+            <NavLink to={"/OneCat/"+props.id}>
+                <h3><p style={{fontSize:16,color:"#8c0494",fontFamily:"Georgia" }}>{props.nickname}</p></h3>
+                <img className="figure-img img-fluid img-thumbnail rounded" src={props.photo_1} alt="cat" style={{width:270,height:380}}/>
             </NavLink>
         </div>
     </div>
@@ -16,7 +16,7 @@ export class Cats extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            posts: [],
+            cats: [],
         }
     }
     componentDidMount() {
@@ -28,32 +28,37 @@ export class Cats extends React.Component{
                     rows.push(<Div
                         key={i}
                         index={i+1}
+                        id={result[i].id}
                         nickname={result[i].nickname}
                         photo_1={result[i].photo_1}
+
                     />)
                 }
                 this.setState({
-                    posts: rows
+                    cats: rows
                 })
             })
     }
     render() {
         return (
-                <section className="cat-list-area section-gap" style={{backgroundImage:"url(https://image.freepik.com/free-vector/cute-paw-footprint-seamless-pattern-background_42349-767.jpg)"}}>>
-                    <div className="text-center">
+                <section className="cat-list-area section-gap"
+                style={{backgroundImage:"url(https://image.freepik.com/free-vector/cute-paw-footprint-seamless-pattern-background_42349-767.jpg)"}}>>
+
+                    <div className="text-center shadow-sm p-3 mb-5 bg-white rounded ">
                         <h1><p style={{fontSize:22,color:"#140395",fontFamily:"Georgia" }}>Сегодня <br />
-                        в Благотворительном фонде "БУДУ РЯДОМ" ждут хозяина:</p></h1>
+                        в БЛАГОТВОРИТЕЛЬНОМ ФОНДЕ "БУДУ РЯДОМ" ждут хозяина:</p></h1>
                         <br />
                     </div>
+
                     <div className="container">
                         <div className="row">
                                 <div className="single-cat-list" >
-                                    <div className="row">{this.state.posts}</div>
+                                    <div className="row">{this.state.cats}</div>
                                 </div>
                         </div>
                     </div>
+
                 </section>
             )
-
     }
 }
--- src/components/OneCat.jsx
+++ src/components/OneCat.jsx
@@ -4,66 +4,77 @@ export class OneCat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: '',
             nickname: '',
             text: '',
-            photo_1: '',
+
             photo_2: '',
             photo_3: '',
             photo_4: '',
             photo_5: ''
         }
     }
-
     componentDidMount() {
+
+        console.log("Компонет OneCat отрисован");
         const formData = new FormData();
-        formData.append('id', 1);
-        {/*formData.append('id', this.state.id);*/}
+        formData.append('id', window.location.pathname.split('/')[2]);
         fetch("http://creatingws.beget.tech/php/getCat.php", {
             method: 'POST',
             body: formData
         })
             .then(response => response.json())
             .then(result => {
-
+                console.log(result);
                 this.setState({
-                    id: result.id,
                     nickname: result.nickname,
                     text: result.text,
-                    photo_1: result.photo_1,
+
                     photo_2: result.photo_2,
                     photo_3: result.photo_3,
                     photo_4: result.photo_4,
                     photo_5: result.photo_5
                 });
+
             })
     }
-
     render() {
         return (
-            <section className="cat-list-area section-gap" style={{backgroundImage:"url(https://image.freepik.com/free-vector/cute-paw-footprint-seamless-pattern-background_42349-767.jpg)"}}>>
-                <div className="text-center">
-                        <h1 style={{fontFamily:"Georgia", color: "#8c0494" }}>
-                           <b>{this.state.nickname}</b><br/>
-                        </h1>
-                        <p style={{fontSize:14, color: "#8c0494", fontFamily: "Georgia", fontStyle: "italic"}}>
+            <section className="cat-list-area section-gap "
+            style={{backgroundImage:"url(https://image.freepik.com/free-vector/cute-paw-footprint-seamless-pattern-background_42349-767.jpg)"}}>>
+                    <h2 class="text-center" style={{color:"#8c0494",fontFamily:"Georgia"}}>
+                        <p> {this.state.nickname}</p>
+                        <br/>
+                    </h2>
+                <div className="container text-center shadow-sm p-3 mb-5 bg-white rounded lh-lg">
+                        <p style={{fontSize:20, color: "#3c3c3c", lineHeight: 2, fontFamily: "Georgia"}}>
                             {this.state.text}
                         </p>
                 </div>
-
-                {/*показ фото*/}
+                <br/>
+                {/*отображаем фото кошек*/}
                 <div className="container">
-                         <div className="single-cat-list" >
-                             <img src={this.state.photo_1} style={{width:262,height:340}}/>
+                    <div className="single-cat-list" >
+                         <div className="row">
+                             <img className="figure-img img-fluid img-thumbnail rounded" src={this.state.photo_1} alt="cat" style={{width:262,height:350}}/>
                              <br/>
-                             <img src={this.state.photo_2} style={{width:262,height:340}}/>
-                             <br/>
-                             <img src={this.state.photo_3} style={{width:262,height:340}}/>
-                             <br/>
-                             <img src={this.state.photo_4} style={{width:262,height:340}}/>
-                             <br/>
-                             <img src={this.state.photo_5} style={{width:262,height:340}}/>
                          </div>
+                    </div>
+
+                             <figure className="figure">
+                                <img src={this.state.photo_2} alt="cat" className="figure-img img-fluid img-thumbnail rounded" style={{width:270,height:380}}/>
+                             </figure>
+                             <figure className="figure">
+                                <img src={this.state.photo_3} alt="cat" className="figure-img img-fluid img-thumbnail rounded" style={{width:270,height:380}}/>
+                             </figure>
+                             <figure className="figure">
+                                <img src={this.state.photo_4} alt="cat" className="figure-img img-fluid img-thumbnail rounded" style={{width:270,height:380}}/>
+                             </figure>
+                             <figure className="figure">
+                                <img src={this.state.photo_5} alt="cat" className="figure-img img-fluid img-thumbnail rounded" style={{width:270,height:380}}/>
+                             </figure>
+                             {/*</div>*/}
+
+                    {/*     </div>*/}
                 </div>
 
             </section>
