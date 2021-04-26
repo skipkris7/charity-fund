ad24f77070ee4dfc2088d80e3d6b5123e2832fd6
Руслан Ситдиков <legolaz2007@mail.ru>
2021-04-26 15:08:03 +0300

    work Post version 1.

--- src/components/OnePost.jsx
+++ src/components/OnePost.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {host} from "../config";
 
 export class OnePost extends React.Component{
-    render(){
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            text: '',
+        }
+    }
+
+    componentDidMount() {
+        const formData = new FormData();
+       formData.append('id', window.location.pathname.split('/')[2]);
+        //formData.append("id", this.props.match.params.id);
+
+        fetch(host + "/getPost", {
+            method: "POST",
+            body: formData
+        })
+        .then (response => response.json())
+            .then (result => {
+                console.log(result);
+                this.setState ({
+                    title: result.title,
+                    text: result.text,
+                })
+            })
+    }
+
+    render() {
         return(
             <section className="top-category-widget-area pt-90 pb-90 ">
                 <div className="container">
@@ -59,6 +88,15 @@ export class OnePost extends React.Component{
                                     </a>
                                 </div>
                             </div>
+                        </div>
+                        <div class="text-center">
+                            <h3 class="text-center">
+                                {this.state.title}
+                            </h3>
+                            <p class="text-center">
+                                {this.state.text}
+                            </p>
+
                         </div>
                     </div>
                 </div>
